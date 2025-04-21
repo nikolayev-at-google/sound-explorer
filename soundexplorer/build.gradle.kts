b3e1fc01346378d2518ad77c9a437227168693dc
Mykola Nikolayev <nikolayev@google.com>
2025-04-21 09:25:54 -0400

    v 0.0.1, update sdk ver

--- soundexplorer/build.gradle.kts
+++ soundexplorer/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 android {
     namespace = "com.google.experiment.soundexplorer"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.google.experiment.soundexplorer"
