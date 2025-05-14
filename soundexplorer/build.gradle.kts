685afb25ceaac7bb1811888f930c6d85d1451640
Mykola Nikolayev <nikolayev@google.com>
2025-05-14 11:27:39 -0400

    source code headers

--- soundexplorer/build.gradle.kts
+++ soundexplorer/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.experiment.jetpackxr.soundexplorer"
         minSdk = 34
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.0.4"
+        versionCode = 100
+        versionName = "0.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
